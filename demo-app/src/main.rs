639b21bdc41c0dc481110a3bc46e13b503dbd3bf
Richard Chien <stdrc@outlook.com>
2022-01-22 16:02:53 +0800

    add simple put

--- demo-app/src/main.rs
+++ demo-app/src/main.rs
@@ -3,4 +3,5 @@ use wasmedge_anna;
 fn main() {
     println!("hello!");
     println!("add result: {}", wasmedge_anna::add(1, 2));
+    wasmedge_anna::put("foo", "bar".as_bytes());
 }
--- wasmedge-anna/src/lib.rs
+++ wasmedge-anna/src/lib.rs
@@ -1,9 +1,25 @@
 extern "C" {
     fn __wasmedge_anna_add(a: i32, b: i32) -> i32;
+    fn __wasmedge_anna_put(
+        key_size: usize,
+        key_ptr: *const u8,
+        val_size: usize,
+        val_ptr: *const u8,
+    ) -> bool;
 }
 
 pub fn add(a: i32, b: i32) -> i32 {
     unsafe { __wasmedge_anna_add(a, b) }
+}
+
+pub fn put(key: impl AsRef<str>, value: impl AsRef<[u8]>) -> bool {
+    let key = key.as_ref();
+    let val = value.as_ref();
+    unsafe { __wasmedge_anna_put(key.len(), key.as_ptr(), val.len(), val.as_ptr()) }
+}
+
+pub fn get(key: impl AsRef<str>) -> Option<Vec<u8>> {
+    todo!()
 }
 
 #[no_mangle]
