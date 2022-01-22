c149b045ddbd868c9c9f160847b58f0741151296
Richard Chien <stdrc@outlook.com>
2022-01-22 17:37:59 +0800

    implement get function

--- demo-app/src/main.rs
+++ demo-app/src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     println!("hello!");
     println!("add result: {}", wasmedge_anna::add(1, 2));
     wasmedge_anna::put("foo", "bar".as_bytes());
+    let val = wasmedge_anna::get("foo");
+    println!("{:?}", val);
+    let val = wasmedge_anna::get("bar");
+    println!("{:?}", val);
 }
--- wasmedge-anna/src/lib.rs
+++ wasmedge-anna/src/lib.rs
@@ -6,6 +6,8 @@ extern "C" {
         val_size: usize,
         val_ptr: *const u8,
     ) -> bool;
+    fn __wasmedge_anna_get_stage_1(key_size: usize, key_ptr: *const u8) -> usize;
+    fn __wasmedge_anna_get_stage_2(key_size: usize, key_ptr: *const u8, val_ptr: *mut u8) -> bool;
 }
 
 pub fn add(a: i32, b: i32) -> i32 {
@@ -19,7 +21,18 @@ pub fn put(key: impl AsRef<str>, value: impl AsRef<[u8]>) -> bool {
 }
 
 pub fn get(key: impl AsRef<str>) -> Option<Vec<u8>> {
-    todo!()
+    let key = key.as_ref();
+    let val_size = unsafe { __wasmedge_anna_get_stage_1(key.len(), key.as_ptr()) };
+    if val_size == 0 {
+        return None;
+    }
+    let mut val = vec![0; val_size];
+    let ok = unsafe { __wasmedge_anna_get_stage_2(key.len(), key.as_ptr(), val.as_mut_ptr()) };
+    if ok {
+        Some(val)
+    } else {
+        None
+    }
 }
 
 #[no_mangle]
