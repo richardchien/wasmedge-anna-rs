daf0c18b9b8cd4c0b16673a8ef3feb3f899fbcc1
Richard Chien <stdrc@outlook.com>
2022-01-25 15:27:06 +0800

    import host functions from `wasmedge_anna` module

--- wasmedge-anna/src/lib.rs
+++ wasmedge-anna/src/lib.rs
@@ -1,22 +1,25 @@
-extern "C" {
-    fn __wasmedge_anna_put(
-        key_size: usize,
-        key_ptr: *const u8,
-        val_size: usize,
-        val_ptr: *const u8,
-    ) -> bool;
-    fn __wasmedge_anna_get(
-        key_size: usize,
-        key_ptr: *const u8,
-        val_buf_size: usize,
-        val_buf_ptr: *mut u8,
-    ) -> usize;
+mod host {
+    #[link(wasm_import_module = "wasmedge_anna")]
+    extern "C" {
+        pub fn put(
+            key_size: usize,
+            key_ptr: *const u8,
+            val_size: usize,
+            val_ptr: *const u8,
+        ) -> bool;
+        pub fn get(
+            key_size: usize,
+            key_ptr: *const u8,
+            val_buf_size: usize,
+            val_buf_ptr: *mut u8,
+        ) -> usize;
+    }
 }
 
 pub fn put(key: impl AsRef<[u8]>, value: impl AsRef<[u8]>) -> bool {
     let key = key.as_ref();
     let val = value.as_ref();
-    unsafe { __wasmedge_anna_put(key.len(), key.as_ptr(), val.len(), val.as_ptr()) }
+    unsafe { host::put(key.len(), key.as_ptr(), val.len(), val.as_ptr()) }
 }
 
 pub fn get(key: impl AsRef<[u8]>) -> Option<Vec<u8>> {
@@ -25,9 +28,8 @@ pub fn get(key: impl AsRef<[u8]>) -> Option<Vec<u8>> {
     let mut val_buf = Vec::new();
 
     loop {
-        val_size = unsafe {
-            __wasmedge_anna_get(key.len(), key.as_ptr(), val_buf.len(), val_buf.as_mut_ptr())
-        };
+        val_size =
+            unsafe { host::get(key.len(), key.as_ptr(), val_buf.len(), val_buf.as_mut_ptr()) };
         if val_size == 0 {
             return None;
         }
