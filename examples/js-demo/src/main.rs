e107c9228cd5a27d77a6486a6d12d326296a5cdc
Richard Chien <stdrc@outlook.com>
2022-02-07 16:26:25 +0800

    provide js api as module

--- examples/js-demo/src/main.rs
+++ examples/js-demo/src/main.rs
@@ -1,57 +1,72 @@
-use wasmedge_anna;
-use wasmedge_quickjs::{Context, JsFn, JsValue};
+use wasmedge_quickjs::Context;
 
-struct PutFn;
-impl JsFn for PutFn {
-    // Put: (key: String | Uint8Array, value: String | Uint8Array) -> bool
-    fn call(_ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
-        if argv.len() != 2 {
-            return JsValue::Bool(false);
+mod js_anna_api {
+    use wasmedge_quickjs::*;
+
+    struct PutFn;
+    impl JsFn for PutFn {
+        // Put: (key: String | Uint8Array, value: String | Uint8Array) -> bool
+        fn call(_ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
+            if argv.len() != 2 {
+                return JsValue::Bool(false);
+            }
+            let key: Vec<u8>;
+            let val: Vec<u8>;
+            match &argv[0] {
+                JsValue::String(s) => key = s.to_string().bytes().collect(),
+                JsValue::ArrayBuffer(u8a) => key = u8a.to_vec(),
+                _ => return JsValue::Bool(false),
+            }
+            match &argv[1] {
+                JsValue::String(s) => val = s.to_string().bytes().collect(),
+                JsValue::ArrayBuffer(u8a) => val = u8a.to_vec(),
+                _ => return JsValue::Bool(false),
+            }
+            return JsValue::Bool(wasmedge_anna::put(key, val));
         }
-        let key: Vec<u8>;
-        let val: Vec<u8>;
-        match &argv[0] {
-            JsValue::String(s) => key = s.to_string().bytes().collect(),
-            JsValue::ArrayBuffer(u8a) => key = u8a.to_vec(),
-            _ => return JsValue::Bool(false),
-        }
-        match &argv[1] {
-            JsValue::String(s) => val = s.to_string().bytes().collect(),
-            JsValue::ArrayBuffer(u8a) => val = u8a.to_vec(),
-            _ => return JsValue::Bool(false),
-        }
-        return JsValue::Bool(wasmedge_anna::put(key, val));
     }
-}
 
-struct GetFn;
-impl JsFn for GetFn {
-    // Get: (key: String | Uint8Array) -> Uint8Array | null
-    fn call(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
-        if argv.len() != 1 {
-            return JsValue::Null;
+    struct GetFn;
+    impl JsFn for GetFn {
+        // Get: (key: String | Uint8Array) -> Uint8Array | null
+        fn call(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
+            if argv.len() != 1 {
+                return JsValue::Null;
+            }
+            let key: Vec<u8>;
+            match &argv[0] {
+                JsValue::String(s) => key = s.to_string().bytes().collect(),
+                JsValue::ArrayBuffer(u8a) => key = u8a.to_vec(),
+                _ => return JsValue::Bool(false),
+            }
+            let val = wasmedge_anna::get(key);
+            val.map_or_else(
+                || JsValue::Null,
+                |v| JsValue::ArrayBuffer(ctx.new_array_buffer(&v)),
+            )
         }
-        let key: Vec<u8>;
-        match &argv[0] {
-            JsValue::String(s) => key = s.to_string().bytes().collect(),
-            JsValue::ArrayBuffer(u8a) => key = u8a.to_vec(),
-            _ => return JsValue::Bool(false),
+    }
+
+    struct Module;
+    impl ModuleInit for Module {
+        fn init_module(ctx: &mut Context, m: &mut JsModuleDef) {
+            m.add_export("put\0", ctx.new_function::<PutFn>("put").into());
+            m.add_export("get\0", ctx.new_function::<GetFn>("get").into());
         }
-        let val = wasmedge_anna::get(key);
-        val.map_or_else(
-            || JsValue::Null,
-            |v| JsValue::ArrayBuffer(ctx.new_array_buffer(&v)),
-        )
+    }
+
+    pub fn init(ctx: &mut Context) {
+        ctx.register_module("wasmedge_anna\0", Module, &["put\0", "get\0"]);
     }
 }
 
 fn main() {
     let mut ctx = Context::new();
-    let mut obj = ctx.new_object();
-    obj.set("put", ctx.new_function::<PutFn>("put").into());
-    obj.set("get", ctx.new_function::<GetFn>("get").into());
-    ctx.get_global().set("wasmedgeAnna", obj.into());
+    js_anna_api::init(&mut ctx);
+
     let code = r#"
+        import * as anna from "wasmedge_anna";
+
         function ab2str(buf) {
             return String.fromCharCode.apply(null, new Uint8Array(buf));
         }
@@ -66,11 +81,13 @@ fn main() {
         }
 
         let value = "bar " + Math.random();
-        print("put result:", wasmedgeAnna.put("foo", str2ab(value)));
-        let foo_val = wasmedgeAnna.get("foo");
+        print("put result:", anna.put("foo", str2ab(value)));
+        let foo_val = anna.get("foo");
         print("foo:", ab2str(foo_val)); // should be "bar " + a random number
-        let bar_val = wasmedgeAnna.get("bar");
+        let bar_val = anna.get("bar");
         print("bar:", bar_val); // should be null
     "#;
-    ctx.eval_global_str(code);
+
+    ctx.eval_module_str(code, "main.js");
+    ctx.promise_loop_poll();
 }
